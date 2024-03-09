262313ec9a7c737e0e1929465ad8b89d6cf29f7e
Praiselouis <123334952+Praiselouis@users.noreply.github.com>
2024-03-09 02:29:21 +0100

    Update 11-createEmployeesObject.js
    obj

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {};
-  obj.$departmentName = [];
-  for (let employee of empoyees) {
-    obj.$departmentName.push(employee);
-  }
-  return obj;
+  return { [departmentName]: [...employees] };
 }
